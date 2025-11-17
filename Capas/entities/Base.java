c2e17d325b4a4cd829ff601f800e25f2120b1949
Franco Adrian Luna Colchicini <francomoon99@gmail.com>
2025-11-17 20:48:10 -0300

    Create Base.java
    Creación de la carpeta de Capas, el paquete de enteties y Clase abstracta Base

---
+++ Capas/entities/Base.java
@@ -0,0 +1,37 @@
+package entities;
+/**
+ *
+ * @author franc
+ */
+public abstract class Base {
+    
+    private int id;
+    private boolean eliminado;
+    
+    protected Base(int id, boolean eliminado) {
+        this.id = id;
+        this.eliminado = eliminado;
+    }
+    
+    protected Base() {
+        this.eliminado = false;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public boolean isEliminado() {
+        return eliminado;
+    }
+    
+    public void setEliminado(boolean eliminado) {
+        this.eliminado = eliminado;
+    }
+
+    
+}
